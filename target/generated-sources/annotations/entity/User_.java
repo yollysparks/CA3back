364f17762bb38f18880897bcbd28e1de1a2732f7
Marcofrydshou1 <marcofrydshou@gmail.com>
2017-11-01 19:27:09 +0100

    bla

--- target/generated-sources/annotations/entity/User_.java
+++ target/generated-sources/annotations/entity/User_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-11-01T19:13:30")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-11-01T19:25:14")
 @StaticMetamodel(User.class)
 public class User_ { 
 
