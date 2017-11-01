7f0feb063578e74a4beda05bfa741b07f0977c7a
Marcofrydshou1 <marcofrydshou@gmail.com>
2017-11-01 19:14:50 +0100

    backend

--- target/generated-sources/annotations/entity/Adress_.java
+++ target/generated-sources/annotations/entity/Adress_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-11-01T18:57:20")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-11-01T19:13:30")
 @StaticMetamodel(Adress.class)
 public class Adress_ { 
 
